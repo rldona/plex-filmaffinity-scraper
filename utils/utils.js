997222a96fc3146a1cc22d1f6ff7ffc5d0a75a52
Raúl López Doña <rldona@gmail.com>
2020-05-25 14:07:38 +0200

    changed fetch size

--- utils/utils.js
+++ utils/utils.js
@@ -22,7 +22,7 @@ exports.convertXMLtoJSON = async (type) => {
   return JSON.parse(mediaList.toString('utf8'));
 };
 
-exports.createMediaPlexInfo = (list, index) => {
+exports.createMediaPlexInfo = async (list, index) => {
   return {
     title: list.elements[0].elements[index].attributes.title || list.elements[0].elements[index].attributes.originalTitle,
     originalTitle: list.elements[0].elements[index].attributes.originalTitle || list.elements[0].elements[index].attributes.title,
